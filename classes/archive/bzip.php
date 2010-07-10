d094a95168a324c56c4d66d7a74045f3024906b0
Matt Button <matthew@sigswitch.com>
2010-07-10 09:36:30 -0700

    Fixes #2

--- classes/archive/bzip.php
+++ classes/archive/bzip.php
@@ -9,7 +9,7 @@
  * @copyright  (c) 2007-2008 Kohana Team
  * @license    http://kohanaphp.com/license.html
  */
-class Archive_Bzip_Driver extends Archive {
+class Archive_Bzip extends Archive {
 
 	public function create($paths, $filename = FALSE)
 	{
